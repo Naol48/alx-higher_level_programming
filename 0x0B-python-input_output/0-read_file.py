32fae057eaf9d57e26a08392847d1f83afc9ef19
naol48 <abu487@gmail.com>
2023-01-10 10:30:38 -0800

    task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-
-
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        lineNum = 1
+        while(True):
+            line = f.readline()
+            if not line:
+                break
+            print(line, end="")
+            lineNum += 1
