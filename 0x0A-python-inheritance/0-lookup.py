2b0451051f90dc04b5c4d2ac7da04c91efd5dbab
naol48 <abu487@gmail.com>
2023-01-10 22:30:38 -0800

    task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+
+d for list available attributtes and methods"""
+
+
+def lookup(obj):
+        """function that returns the list of available attributes
+            and methods of an object"""
+
+                return dir(obj)
