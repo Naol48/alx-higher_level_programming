3028e48a9a3b66436b59e544eb341e4807d610fd
naol48 <abu487@gmail.com>
2023-01-11 09:42:47 -0800

    task

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+"""Class definition"""
+
+
 def is_same_class(obj, a_class):
-    """
-    Checks if `obj` is exactly an instance of the specified class
-    Args:
-        obj (any): The object to compare
-        a_class (any): The class to compare with the object
-    Returns:
-        `True` if the object is exactly an instance of the
-        specified class; otherwise `False`
-    """
-
-    if type(obj) == a_class:
-        return True
-
-    return False
+    """Return True if the object is exactly an instance
+    of the specified class, otherwise False"""
+    return(type(obj) is a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Class definition"""
+
+
+class BaseGeometry:
+    """This is a empty
